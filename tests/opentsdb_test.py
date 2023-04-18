21a2868e9e525e2d007e769607f45a628d54bd6f
Niels Westphal <git@nielswestphal.de>
2023-04-18 14:00:12 +0200

    test tests

--- tests/opentsdb_test.py
+++ tests/opentsdb_test.py
@@ -1,4 +1,4 @@
-""" import unittest
+import unittest
 import opentsdb.query
 
 import opentsdb.request
@@ -7,9 +7,15 @@ class TestRequestBuilder(unittest.TestCase):
     def setUp(self) -> None:
         self.request_builder_valid = opentsdb.request.RequestBuilder()
 
+    def assert_true(self):
+        self.assertTrue(True)
+
 class TestQueryBuilder(unittest.TestCase):
     def setUp(self) -> None:
         self.query_builder_valid = opentsdb.query.QueryBuilder()
+
+    def assert_true(self):
+        self.assertTrue(True)
 
 class TestRateOptions(unittest.TestCase):
     def setUp(self) -> None:
@@ -17,12 +23,11 @@ class TestRateOptions(unittest.TestCase):
         self.rate_options_defaults_valid = opentsdb.query.RateOptions(drop_resets=True, reset_value=1000)
 
     def test_all_values_given(self):
-        self.assertEqual(self.rate_options_valid.__str__(), "{False,100,1000}")
-        self.assertEqual(self.rate_options_defaults_valid.__str__(), "{,,1000}")
+        self.assertTrue(True)
 
 class TestFilter(unittest.TestCase):
     def setUp(self) -> None:
         self.filter_valid = opentsdb.query.Filter(type="wildcard", tagk="tagk", filter="filter", group_by=False)
 
     def test_all_values_given(self):
-        self.assertEqual(self.filter_valid.__str__(), "tagk=wildcard(filter)") """
+        self.assertTrue(True)
